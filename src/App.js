609e823d5857d53b1f569184829a18a404bf2106
TK-wsh <ghr3ndkdy@gmail.com>
2023-09-09 16:40:26 +0900

    create database

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
 import "./App.css";
-import "@aws-amplify/ui-react/styles.css"; //ここを追加
+import "@aws-amplify/ui-react/styles.css";
 
-import { withAuthenticator } from "@aws-amplify/ui-react"; //ここを追加
-import { Amplify } from "aws-amplify"; //ここを追加
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
 
-import aws_exports from "./aws-exports"; //ここを追加
-import logo from "./logo.svg";
+import aws_exports from "./aws-exports";
+import { DynamoDB } from "./components/DynamoDB"; //ここを追加
 
-Amplify.configure(aws_exports); //ここを追加
+Amplify.configure(aws_exports);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* 下記を変更 */}
+        <DynamoDB />
       </header>
     </div>
   );
 }
 
-export default withAuthenticator(App); //ここを変更
+export default withAuthenticator(App);
